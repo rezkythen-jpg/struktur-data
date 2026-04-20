9a28b0e8832dfe5685b2b2769249ae3a76cd8a50
Lalu muhamad rezky pradita <rezkythen@gmail.com>
2026-04-20 09:11:08 +0800

    Add files via upload

---
+++ meet2resky.py
@@ -0,0 +1,16 @@
+# perulangan (LOOP)
+# for kondisi :
+
+angka = [0,1,2,3,4] # ini menggunakan list
+
+for i in angka :
+    print(f"nilai i = {i}")
+    print("resky")
+print("program 1 selesai selesai \n")
+
+angka = range(1,11) #ini menggunakan range
+
+for i in angka :
+    print(f"nilai i = {i}")
+    print("resky")
+print("program 1 selesai")
